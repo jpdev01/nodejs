6dd5a654676933a070105e60b869bb03c43d58b3
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-08-29 15:33:13 -0300

    init project

---
+++ readme.md
@@ -0,0 +1,6 @@
+/*url starter: https://github.com/andrewarosario/node-typescript-starter*/
+
+npm i express cors (para o angular usar a api) mongoose (ajuda a ligar com o mongodb)
+como usamos typescript, vamos adicionar a tipagem necessária para essas libs:
+
+npm i @types/express @types/cors @types/mongoose
---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+export class App {
+    private express: express.Application;
+    private porta = 9000;
+
+    constructor() {
+        this.express = express();
+    }
+
+    private listen(): void {
+        this.express.listen(this.porta, () => {
+            console.log("Servidor iniciado na porta " + this.porta);
+        })
+    }
+}
