540bbd0cb731d2bc6b0ce7dbe027fe5089cf6f8b
Joao Pedro Truchinski Borba <jptruchinski@gmail.com>
2021-08-29 16:41:14 -0300

    controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
+import userModel from "../models/user.model";
 
 class UserController {
 
     public register(req: Request, resp: Response){
-
+        // create ja vem do mongoouse, cria registro, metodo pronto
+        const user = userModel.create(req.body);
+        return resp.json(user);
     }
 }
 
